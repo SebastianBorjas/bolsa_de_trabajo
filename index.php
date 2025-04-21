92a090420c70a4782989096b0898cb59685e57df
SebastianBorjas <panfilacaches@gmail.com>
2025-04-21 12:11:57 -0600

    ediciones movil

--- index.php
+++ index.php
@@ -4,7 +4,7 @@
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Iniciar Sesión</title>
-    <link rel="stylesheet" href="estilos.css">
+    <link rel="stylesheet" href="estilos.css?v=<?php echo time(); ?>">
     <link href="https://fonts.googleapis.com/css2?family=Roboto:wght@400;500;600;700&display=swap" rel="stylesheet"> <!-- Para Roboto -->
 </head>
 <body>
