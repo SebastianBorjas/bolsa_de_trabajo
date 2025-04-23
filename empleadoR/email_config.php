953dce0eec972bafe8205acdcc8d88b8594cec10
SebastianBorjas <panfilacaches@gmail.com>
2025-04-23 11:42:03 -0600

    asldk

---
+++ empleadoR/email_config.php
@@ -0,0 +1,12 @@
+<?php
+// moderador/config/email_config.php
+return [
+    'smtp_host' => 'smtp.titan.email',
+    'smtp_username' => 'jefecapacitacion@canacintramonclova.org',
+    'smtp_password' => '&3c@^t#83e%2',
+    'smtp_secure' => 'tls',
+    'smtp_port' => 587,
+    'from_email' => 'jefecapacitacion@canacintramonclova.org',
+    'from_name' => 'Bolsa de Trabajo'
+];
+?>
